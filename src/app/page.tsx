5288782fe76c804201163b2f3e723b1b752b068d
Tobias Poulsen (Biast12) <tobiaspoulsen@hotmail.com>
2024-08-15 13:10:54 +0200

    Update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
   useEffect(() => {
     (async () => {
       const { data, error } = await simpleFetch<DataItem[]>(
-        process.env.NEXT_PUBLIC_HOST + "/api/data/page3"
+        process.env.NEXT_PUBLIC_HOST + "/api/data/homepage"
       );
 
       setData(data);
